e47f0bb046f6834b88206f548ff8bc22924191d2
malfois <malfois@cells.es>
2021-03-18 17:48:56 +0100

    Works on linux

--- es.alba.sweet.base/src/es/alba/sweet/base/constant/CommandLine.java
+++ es.alba.sweet.base/src/es/alba/sweet/base/constant/CommandLine.java
@@ -10,7 +10,8 @@ public enum CommandLine {
 	private List<String> commandLine = new ArrayList<>();
 
 	private CommandLine(String type) {
-		if (type.equals("server")) initiliaseServer();
+		if (type.equals("server"))
+			initiliaseServer();
 	}
 
 	public List<String> get() {
@@ -19,14 +20,11 @@ public enum CommandLine {
 
 	private void initiliaseServer() {
 		String os = System.getProperty("os.name");
-		if (os.equals("Linux")) {
-			commandLine.add("/beamlines/bl11/controls/Marc/jdk-11.0.2/bin/java");
-			commandLine.add("-jar");
-			commandLine.add("/beamlines/bl11/controls/Marc/javaRCP/jar/SweetServer.jar");
-		}
 		if (os.startsWith("Windows")) {
 			this.createWindowsCommandLine();
+			return;
 		}
+		this.createLinuxCommandLine();
 	}
 
 	private void createWindowsCommandLine() {
@@ -35,6 +33,38 @@ public enum CommandLine {
 		commandLine.add("-classpath");
 		commandLine.add(Windows.CLASSPATH.get());
 		commandLine.add("es.alba.sweet.server.SweetServer");
+	}
+
+	private void createLinuxCommandLine() {
+		commandLine.add("/beamlines/bl11/controls/Marc/jdk-11.0.2/bin/java");
+		commandLine.add("-Dfile.encoding=UTF-8");
+		commandLine.add("-classpath");
+		commandLine.add(Linux.CLASSPATH.get());
+		commandLine.add("es.alba.sweet.server.SweetServer");
+	}
+
+	private enum Linux {
+
+		CLASSPATH();
+
+		private String classPath;
+
+		private Linux() {
+			String pluginsDirectory = "/homelocal/opbl11/.p2/pool/plugins/";
+			String baseDirectory = "/beamlines/bl11/controls/Marc/git/es.alba.sweet.base/es.alba.sweet.base/bin:";
+			String serverDirectory = "/beamlines/bl11/controls/Marc/git/es.alba.sweet.server/es.alba.sweet.server/bin";
+
+			classPath = baseDirectory;
+			classPath = classPath + pluginsDirectory + "com.fasterxml.jackson.core.jackson-annotations_2.10.3.v20200512-1600.jar:";
+			classPath = classPath + pluginsDirectory + "com.fasterxml.jackson.core.jackson-core_2.10.3.v20200512-1600.jar:";
+			classPath = classPath + pluginsDirectory + "com.fasterxml.jackson.core.jackson-databind_2.10.3.v20200512-1600.jar:";
+			classPath = classPath + pluginsDirectory + "org.apache.commons.math3_3.5.0.v20160301-1110.jar:";
+			classPath = classPath + serverDirectory;
+		}
+
+		public String get() {
+			return this.classPath;
+		}
 	}
 
 	private enum Windows {
